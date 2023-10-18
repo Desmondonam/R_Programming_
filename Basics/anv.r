d44fdd49f99bd5a1dc6b46fefd31418276324601
Desmond Onam <desmondonam@gmail.com>
2023-10-18 11:08:18 +0300

    add scaling

--- Basics/anv.r
+++ Basics/anv.r
@@ -1,8 +1,13 @@
-# Sample data
-group1 <- c(25, 30, 35, 40, 45)
-group2 <- c(20, 22, 25, 27, 30)
-group3 <- c(15, 18, 20, 23, 25)
+# Create sample data for a two-way ANOVA
+factor1 <- factor(rep(1:2, each = 15))
+factor2 <- factor(rep(1:3, times = 10))
+values <- rnorm(30)
 
-# combine the data into a single data Frame
+# Combine the data into a data frame
+data <- data.frame(Factor1 = factor1, Factor2 = factor2, Value = values)
 
-data <- data.frame(Value)
+# Perform a two-way ANOVA
+anova_result <- aov(Value ~ Factor1 * Factor2, data = data)
+
+# Summarize the ANOVA results
+summary(anova_result)
