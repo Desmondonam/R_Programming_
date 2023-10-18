959c1077fc5d06fef4747f8194e0f43ecde490a8
Desmond Onam <desmondonam@gmail.com>
2023-10-18 11:01:39 +0300

    add anova

---
+++ Basics/anv.r
@@ -0,0 +1,8 @@
+# Sample data
+group1 <- c(25, 30, 35, 40, 45)
+group2 <- c(20, 22, 25, 27, 30)
+group3 <- c(15, 18, 20, 23, 25)
+
+# combine the data into a single data Frame
+
+data <- data.frame(Value)
